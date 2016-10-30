23c7c7ab862904ca8dcdf5ed63734c565cc7f7cf
Lorie <loriekh10@gmail.com>
2016-10-30 00:23:47 -0400

    Completed the revea on scroll behaviour.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/mobilemenu';
+inport RealOnScroll from './modules/realonscroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
